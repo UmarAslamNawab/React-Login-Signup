7e5038a3f24ff0624046f2f502eeaa27684f3fef
UmarAslamNawab <umar_uni321@outlook.com>
2018-01-01 02:49:11 -0800

    React Login-Signup

---
+++ src/Authentication/components/index.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react'
+// import 'bootstrap/dist/css/bootstrap.css'
+import { Route, BrowserRouter, Link, Redirect, Switch } from 'react-router-dom'
+import Login from './Login'
+import Register from './Register'
+import Home from './Home'
+import Dashboard from './protected/Dashboard'
+import { logout } from '../helpers/auth'
+import { firebaseAuth } from '../config/constants';
+
+
+function PrivateRoute ({component: Component, authed, ...rest}) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authed === true
+        ? <Component {...props} />
+        : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}
+    />
+  )
+}
+
+function PublicRoute ({component: Component, authed, ...rest}) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authed === false
+        ? <Component {...props} />
+        : <Redirect to='/dashboard' />}
+    />
+  )
+}
+
+export default class App extends Component {
+  state = {
+    authed: false,
+    loading: true,
+  }
+  componentDidMount () {
+    this.removeListener = firebaseAuth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authed: true,
+          loading: false,
+        })
+      } else {
+        this.setState({
+          authed: false,
+          loading: false
+        })
+      }
+    })
+  }
+  componentWillUnmount () {
+    this.removeListener()
+  }
+  render() {
+    return this.state.loading === true ? <h1>Loading</h1> : (
+      <BrowserRouter>
+        <div>
+          <div className="navbar navbar-default navbar-static-top " style={{backgroundColor:'white' }}>
+            <div className="container">
+              <div className="navbar-header">
+                <Link to="/" className="navbar-brand" ><h4>Todo App With Firebase Authentication</h4></Link>
+              </div>
+              <ul className="nav navbar-nav pull-right">
+                <li>
+                  <Link to="/" className="navbar-brand">Home</Link>
+                </li>
+                <li>
+                  <Link to="/dashboard" className="navbar-brand">Dashboard</Link>
+                </li>
+                <li>
+                  {this.state.authed
+                    ? <button className="waves-effect waves-light btn"
+                        // style={{border: 'none', background: '' , }}
+                        onClick={() => {
+                          logout()
+                        }}
+                        >Logout</button>
+                      
+                 
+                       
+                    : <span>
+                        <Link to="/login" className=" waves-effect waves-light btn">Login</Link>
+                        <Link to="/register" className="waves-effect waves-light btn">Register</Link>
+                      </span>
+                    
+                    }
+                       </li>
+                        </ul>
+                       </div>
+                     </div> 
+          <div className="container">
+            <div className="row">
+              <Switch>
+                <Route path='/' exact component={Home} />
+                <PublicRoute authed={this.state.authed} path='/login' component={Login} />
+                <PublicRoute authed={this.state.authed} path='/register' component={Register} />
+                <PrivateRoute authed={this.state.authed} path='/dashboard' component={Dashboard} />
+                <Route render={() => <h3>No Match</h3>} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+// style={{paddingLeft:'100px'}}
